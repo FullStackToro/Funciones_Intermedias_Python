97c723024b4d126db8c7a791711960afea9097aa
FullStackToro <g.toro.guillon@gmail.com>
2020-12-17 20:22:21 -0300

    segundo commit

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ def beCheerful(name='', repeat=2):  # set defaults when declaring the parameters
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-  randInt(99)
+  randInt(199)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
