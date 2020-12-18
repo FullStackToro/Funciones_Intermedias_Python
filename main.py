61eae43cdc0a7455cebf759f6058ef1739696bee
FullStackToro <g.toro.guillon@gmail.com>
2020-12-17 21:20:12 -0300

    Se implementa codigo de la actividad

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 
 import random
 def randInt (min=0,max=100):
-    num=random.random()*(max-min)+min
-    num=round(num)
-    print(num)
+    if min>max:
+
+        return 'Error: Verificar valor mínimo y máximo.'
+    elif max<0:
+        return 'Error: El valor máximo no puede ser menor a cero (0).'
+    else:
+        num=random.random()*(max-min)+min
+        num=round(num)
+        return num
 
 
 def beCheerful(name='', repeat=2):  # set defaults when declaring the parameters
@@ -13,6 +19,10 @@ def beCheerful(name='', repeat=2):  # set defaults when declaring the parameters
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-  randInt(1299)
+  print(randInt())
+  print('max50: ',randInt(max=50))
+  print('min50: ', randInt(min=50))
+  print('min50max500:', randInt(min=50,max=500))
+  print('min500max50:', randInt(min=500, max=50))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
